62eb3012f666e42b605be4f18ff635451adf1847
Ben Bowles <55298859+HoleInOneGolfer@users.noreply.github.com>
2022-10-07 13:07:37 -0500

    update

--- build.py
+++ build.py
@@ -2,17 +2,62 @@ import os
 
 import markdown
 
-converted = 0
-total_files = 0
 
-mds = os.listdir('md')
-total_files = len(mds)
-print(mds)
+class colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    NORMAL = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-for md in mds:
-    markdown.markdownFromFile(input='md/' + md, output='src/views/' + md[:-3] + '.html', encoding='utf-8')
-    converted += 1
-    print('Converted ' + md + ' to html' + ' (' + str(converted) + '/' + str(total_files) + ')')
+
+def Convert_To_Markdown():
+    converted = 0
+    total_files = 0
+    
+    mds = os.listdir('md')
+    total_files = len(mds)
+    
+    for md in mds:
+        markdown.markdownFromFile(input='md/' + md, output='src/build/views/' + md[:-3] + '.html', encoding='utf-8')
+        converted += 1
+        # == Progress Indication ==
+        print('Converted md/' + md + ' to src/build/views/' + md[:-3] + '.html (' + str(converted) + '/' + str(total_files) + ')')
+    
+    # == Finished Indication ==
+    print('Finished converting ' + str(converted) + ' files')
+
+def Get_HTML_Index():
+    html_main = ""
+    html_nav = ""
+
+    for file in os.listdir('src/build/views'):
+        if file.endswith('.html'):
+            section = "<section id=\""+os.path.splitext( ( os.path.basename(file) ) )[0]+"\">\n"
+            with open('src/build/views/' + file, 'r') as f:
+                content = f.read()
+                section += content + "\n</section>\n"
+
+            # == Add to main ==
+            html_main += section
+            # == Add to nav ==
+            html_nav += '<a href="#' + os.path.splitext( ( os.path.basename(file) ) )[0] + '">' + file[:-5].capitalize() + '</a>\n'
+
+    # == Finished Main Indication ==
+    print('Finished build HTML main content')
+    open('src/build/main.html', 'w').write(html_main)
+
+    # == Finished Nav Indication ==
+    print('Finished build HTML nav content')
+    open('src/build/nav.html', 'w').write(html_nav)
+
+if __name__ == '__main__':
+    Convert_To_Markdown()
+    Get_HTML_Index()
 
 
 
--- md/courses.md
+++ md/courses.md
@@ -4,8 +4,4 @@
 
 **Computer Science I Pre-AP (H3005L):** Course introduces students to programming with several web page languages including HTML, CSS, & JavaScript.  Students are then introduced to beginning programming with Python and an introduction to Java.
 
-**Computer Science II Pre-AP (H3006L):** This course continues with an introduction to Java & learning about Object Oriented Programming.  This course emphasizes data structures and problem solving with Java which provides a necessary foundation for the next course -- AP Computer Science A.
-
 **AP Computer Science A (H3010L):** This is the last of the programming courses and is "equivalent to a first-semester, college-level course in computer science.  The course emphasizes both object-oriented and imperative problem solving and design using Java language."(APCentral) This course teaches advanced programming and emphasizes object-oriented programming methodology with an emphasis on problem solving and algorithm development.  This course prepares students to take the Advanced Placement (AP) exam that can be used for college credit.
-
-**AP Computer Science Principles (H3007L):** "AP Computer Science Principles offers a multidisciplinary approach to teaching the underlying principles of computation. The course will introduce students to the creative aspects of programming, abstractions, algorithms, large data sets, the Internet, cybersecurity concerns, and computing impacts." (APCentral) Students are expected to make an application that is submitted to the College Board along with the written test to received Advanced Placement credit.  This course is independent of the other Computer Science courses so can be taken at any time.
